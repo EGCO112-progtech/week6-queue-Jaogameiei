e02ef11043fe866a565d5b2c4910d1bbaedfd0fd
Kornchanok <kornchanok.phu@student.mahidol.edu>
2025-02-12 08:27:44 +0000

    add

--- Queue.h
+++ Queue.h
@@ -1,39 +1,47 @@
 #include "Node.h"
 
 typedef struct {
-	 NodePtr headPtr,tailPtr;
-	int size;
-}Queue;
+    NodePtr headPtr, tailPtr;
+    int size;
+} Queue;
 
+void enqueue_struct(Queue* q, int x) {
+    NodePtr new_node = (NodePtr)malloc(sizeof(Node));
+    if (new_node) {
 
-void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-  //NodePtr new_node=(NodePtr)malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
-  new_node->data = x;
-  new_node->nextPtr=NULL;
-  if(q->size==0)  q->headPtr = new_node;/*first Node*/
-  else q->tailPtr->nextPtr = new_node;
-  q->tailPtr = new_node;
-  q->size++;
- }
+        new_node->data = x;
+        new_node->nextPtr = NULL;
+
+        if (q->size == 0) {
+            q->headPtr = new_node;
+        } else {
+
+            q->tailPtr->nextPtr = new_node;
+        }
+
+        q->tailPtr = new_node;
+        q->size++;
+    }
 }
 
+int dequeue_struct(Queue* q) {
+    if (q->size > 0) {
+        NodePtr t = q->headPtr;
+        int value = t->data;
 
-int dequeue_struct(Queue *q){
-  if(q->size>0){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-       q->headPtr= t->nextPtr; // NULL
-        if(q->size==1) q->tailPtr=NULL;    
-  free(t);
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
-   q->size--;
-}
+        q->headPtr = t->nextPtr;
+
+        if (q->size == 1) {
+            q->tailPtr = NULL;
+        }
+
+        free(t);
+
+        q->size--;
+
+        return value;
+    } else {
+        printf("Empty queue"\n");
+        return -1;  
+    }
 }
--- main.c
+++ main.c
@@ -19,8 +19,10 @@ int main(int argc , char **argv) {
 
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-             x = dequeue_struct(&q);
+            x = dequeue_struct(&q);
+            if(x!=-1) 
             printf("dequeing %d\n",x);
+
         }
         else {
        enqueue_struct(&q, atoi(argv[i]));
